78b0802030c7e3067440d9ae39e1635c62b5c07f
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-16 15:33:58 +0530

    Add files via upload

---
+++ greater.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter a:";
+    cin>>a;
+    cout<<"Enter b:";
+    cin>>b;
+
+    if(a>b)
+    {
+        cout<<"A is greater."<<endl;
+    }
+
+    else{
+        cout<<"B is greater."<<endl;
+    }
+
+    return 0;
+}
