2d854cb38b1b745f69025bd02c61f4d42dfd259f
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-17 20:48:37 +0530

    Add files via upload

---
+++ Day 2/areOFcircle.cpp
@@ -0,0 +1,11 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    float pi=3.14, area, r;
+//    cout<<"Enter the radius :";
+//    cin>>r;
+//    area = pi*r*r;
+//    cout<<"The area of circle ="<<area;
+//    return 0;
+// }
