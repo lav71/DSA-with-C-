1cd1c6c36c9a1226d51845ab49471589e827a80a
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-17 20:41:07 +0530

    Add files via upload

---
+++ Codes/Day 4/areaofSphere.cpp
@@ -0,0 +1,11 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     float p=3.14;
+//     float r;
+//     cout<<"Enter radius: ";
+//     cin>>r;
+//     cout<<"The area of sphere is: ";
+//     cout<<4*p*r*r;
+// }
