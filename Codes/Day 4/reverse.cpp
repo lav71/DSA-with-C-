a66caab95ad47d95b44e099cc7705829580c9c31
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-17 20:40:32 +0530

    Add files via upload

---
+++ Codes/Day 4/reverse.cpp
@@ -0,0 +1,18 @@
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     int r=0;
+//     while(n!= 0)
+//     {
+//         int ld = n%10 ;
+//         r=r*10;
+//         r = r+ld;
+//         n=n/10;
+//     }
+//     cout<<r;
+
+// }
