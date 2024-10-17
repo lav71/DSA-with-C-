6384b126efe80f009fa1fa8a6d2b1acd0995e32f
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-17 20:49:01 +0530

    Add files via upload

---
+++ Day 2/areaOFsquare.cpp
@@ -0,0 +1,11 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int s, area;
+//     cout<<"enter the side :";
+//     cin>>s;
+//     area = s*s;
+//     cout<<area<<endl;
+//     return 0;
+// }
