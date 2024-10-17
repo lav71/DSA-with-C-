ab370a93fe3d869c8505e43d211bcf4564877326
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-17 20:47:03 +0530

    Add files via upload

---
+++ Day 2/sumOfDigits.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    int sum = 1;
+    while(n!= 0)
+    {
+        int ld = n%10 ;
+        n=n/10;
+        sum = sum+ld;
+    }
+    cout<<sum;
+
+}
+
+
