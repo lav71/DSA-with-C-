a8b239c69bf6f5fa9e612792742c8f7fe19c3978
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-19 22:22:59 +0530

    Add files via upload

---
+++ Day 6/cost.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     float p;
+//     cout<<"Enter the cost of pencile :";
+//     cin>>p;
+//     float e;
+//     cout<<"Enter the cost of eraser :";
+//     cin>>e;
+//     float pe;
+//     cout<<"Enter the cost of pen :";
+//     cin>>pe;
+//     float  total,GST;
+//     GST = (p+ e + pe)*18/100;
+//     total = (p+e+pe)+GST;
+//     cout<<"Total cost = "<<total<<endl;
+    
+//     return 0;
+// }
