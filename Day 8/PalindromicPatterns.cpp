7ab99cccfdf75d0f9835076367c75bddfe9a1910
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-23 00:07:38 +0530

    Add files via upload

---
+++ Day 8/PalindromicPatterns.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n-i; j++)
+        {
+            cout<<" ";
+        }
+
+        // for backward num
+        for(int j=i; j>=1; j--)
+        {
+            cout<<j;
+        }
+
+        // for forward num
+
+        for(int j=2; j<=i; j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
