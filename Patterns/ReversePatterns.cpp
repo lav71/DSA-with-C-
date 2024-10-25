2b607c2f784e5ac9bc223ba7ff0cc47526888d2d
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-25 23:15:54 +0530

    Add files via upload

---
+++ Patterns/ReversePatterns.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+    for(int i=n; i>=1; i--)
+    {
+        for(int j=n; j>=1; j--)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
