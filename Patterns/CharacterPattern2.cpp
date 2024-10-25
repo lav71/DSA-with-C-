98e88c38191c9f1adc7bf1dee1c18e1ffc76b7bd
Lavkush Vishwakarma <lavkush113322@gmail.com>
2024-10-25 23:41:18 +0530

    Add files via upload

---
+++ Patterns/CharacterPattern2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+    char i,j;
+    for(i='A'; i<='E'; i++)
+    {
+        for( j='A'; j<='E'; j++)
+        {
+           cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
